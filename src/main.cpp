606804ce2684e9082242f49f2a633b613115ee68
Luca Mazzoleni <lmazzole@hsr.ch>
2019-03-08 16:45:18 +0100

    added Doxygen cmt

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
+/**
+ * @file main.cpp
+ * @author Luca Mazzoleni (luca.mazzoleni@hsr.ch)
+ * @brief Main-File for SmartFactoryArm
+ * @version 0.1
+ * @date 2019-03-08
+ * 
+ * 
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
 #include <Arduino.h>
 
+/**
+ * @brief for initialisation of the Board
+ * 
+ * Use it to initialize variables, pin modes, start using libraries, etc.
+ * The setup() function will only run once,
+ * after each powerup or reset of the board
+ */
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
 }
 
+/**
+ * @brief one loop per cycle (SB full -> transported -> returned empty)
+ * 
+ * After creating a setup() function, which initializes and sets the initial values,
+ * the loop() function does precisely what its name suggests,
+ * and loops consecutively, allowing your program to change and respond.
+ * Use it to actively control the board.
+ */
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 }
