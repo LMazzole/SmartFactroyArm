d042355c7cd1e8bc28960ce266a39bfc4d8d2a5f
Luca Mazzoleni <luca.mazzoleni@hsr.ch>
2019-04-11 15:37:18 +0200

    updated global position

---
+++ lib/Configuration/PositionConfiguration.h
@@ -0,0 +1,12 @@
+#ifndef POSITIONCONFIGURATION_H__
+#define POSITIONCONFIGURATION_H__
+
+#define RESTPOSITION -90         // int, where to rest bevore loading
+#define LOADPOSITION -18         // int, where to get until thing is loaded (sensor recognizes thing)
+#define DRIVEPOSITION 50        // int, where to rest for driving with thing loaded
+#define UNLOADPOSITIONLEFT 165   // int, drive until there to eject thing
+#define UNLOADPOSITIONRIGHT -165  // int, drive until there to eject thing
+#define SERVOSPEED 10
+// #define RAMPSIDE true           // true for left, false for right loading ramp
+
+#endif
