4c5c491501042a40aaecb1b6d5153147a661f454
Luca Mazzoleni <lmazzole@hsr.ch>
2019-04-11 13:05:14 +0200

    implemented network class

--- lib/Network/Network.cpp
+++ lib/Network/Network.cpp
@@ -1,107 +1,136 @@
 #include "Network.h"
 
-Network::Network(String name)
-{
-	DBINFO1ln("Initialising the Bluefruit LE module: ");
+Network::Network() : ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST) {
+}
 
-    //ble = new Adafruit_BluefruitLE_SPI(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
-
-  if ( !ble->begin(VERBOSE_MODE) )
-  {
-    DBINFO1ln("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?");
-  }
-
-  if ( FACTORYRESET_ENABLE )
-  {
-    /* Perform a factory reset to make sure everything is in a known state */
-    DBINFO1ln("Performing a factory reset: ");
-    if ( ! ble->factoryReset() ){
-      DBINFO1ln("Couldn't factory reset");
+void Network::init(String name) {
+    DBFUNCCALLln("Initialising the Bluefruit LE module: ");
+    if (!(ble.begin(VERBOSE_MODE))) {
+        DBINFO1ln("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?");
     }
-    ble->verbose(false); // debug info is a little annoying after this point!
-    while (! ble->isConnected()) 
-    {
-        DBINFO1ln("connecting to Bluetooth...");
-        delay(500);
+
+    if (FACTORYRESET_ENABLE) {
+        /* Perform a factory reset to make sure everything is in a known state */
+        DBINFO1ln("Performing a factory reset: ");
+        if (!ble.factoryReset()) {
+            DBERROR("Couldn't factory reset");
+        }
+        ble.verbose(false);  // debug info is a little annoying after this point!
+        while (!ble.isConnected()) {
+            DBINFO1ln("connecting to Bluetooth...");
+            delay(500);
+        }
     }
-  }
 }
 
+Network::receiveStates Network::receiveAndAnalyse() {
+    DBFUNCCALLln("Network::receiveAndAnalyse");
+    ble.println("AT+BLEUARTRX");
+    ble.readline();
+    // DBINFO1ln(ble.buffer);
+    if (strcmp(ble.buffer, "OK") == 0) {
+        // no data
+        return receiveStates::nothingReceived;
+    }
+    DBINFO1ln(ble.buffer);
 
-Network::receiveStates Network::receiveAndAnalyse()
-//enum receiveStates Network::receiveAndAnalyse()
-{
-    DBINFO1ln("entering Network::receiveAndAnalyse");
-    ble->println("AT+BLEUARTRX");
-     ble->readline();
-     DBINFO1ln(ble->buffer);
-    if (strcmp(ble->buffer, "OK") == 0) {
-    // no data
-       return receiveStates::nothingReceived;
-     }
-    // Serial.println(ble->buffer);
-    
-	//TODO ask/wait for message
-	String msg = ble->buffer; // received message
-	if(msg == "pickup(0)")
-		return receiveStates::pickupLeft;
-	else if(msg == "pickup(1)")
-		return receiveStates::pickupRight;
-	else if(msg == "drop(0)")
-		return receiveStates::dropLeft;
-	else if(msg == "drop(1)")
-		return receiveStates::dropRight;
-	else
-		DBINFO1ln("unknown message"); // TODO log unknown message received
-	
-	
+    //TODO ask/wait for message
+    String msg = ble.buffer;  // received message
+    if (msg == "pickup(0)")
+        return receiveStates::pickupLeft;
+    else if (msg == "pickup(1)")
+        return receiveStates::pickupRight;
+    else if (msg == "drop(0)")
+        return receiveStates::dropLeft;
+    else if (msg == "drop(1)")
+        return receiveStates::dropRight;
+    else {
+        DBINFO1ln("unknown message");  // TODO log unknown message received
+        return receiveStates::nothingReceived;
+    }
 }
 
-bool Network::sendMessage(enum Network::sendStates state)
-{
-    DBINFO1ln("enter Network::sendMessage");
-	switch(state)
-	{
-		case 0:
-				sendString("pickupSuccess(1)");
-				break;
-		case 1:
-				sendString("pickupSuccess(0)");
-				break;
-		case 2:
-				sendString("dropSuccess(1)");
-				break;
-		case 3:
-				sendString("dropSuccess(0)");
-				break;
-		default:
-				DBINFO1ln("unknown state");
-	}
+bool Network::sendMessage(enum Network::sendStates state) {
+    DBFUNCCALLln("Network::sendMessage");
+    switch (state) {
+        case 0:
+            sendString("pickupSuccess(1)");
+            break;
+        case 1:
+            sendString("pickupSuccess(0)");
+            break;
+        case 2:
+            sendString("dropSuccess(1)");
+            break;
+        case 3:
+            sendString("dropSuccess(0)");
+            break;
+        default:
+            DBINFO1ln("unknown state");
+    }
 }
 
-bool Network::sendTestMessage(String msg)
-{
+bool Network::sendTestMessage(String msg) {
     sendString(msg);
 }
 
-bool Network::sendString(String msg)
-{
-    ble->print("AT+BLEUARTTX=");
-  ble->println(msg);
+bool Network::sendString(String msg) {
+    ble.print("AT+BLEUARTTX=");
+    ble.println(msg);
 
-  // check response stastus
-  if (! ble->waitForOK() ) {
-    DBINFO1ln("Failed to send?");
+    // check response stastus
+    if (!ble.waitForOK()) {
+        DBINFO1ln("Failed to send?");
     }
 }
 
-
-void Network::printInfo()
-{
+void Network::printInfo() {
     /* Disable command echo from Bluefruit */
-    ble->echo(false);
+    ble.echo(false);
 
-  DBINFO1ln("Requesting Bluefruit info:");
-  /* Print Bluefruit information */
-    ble->info();
+    DBINFO1ln("Requesting Bluefruit info:");
+    /* Print Bluefruit information */
+    ble.info();
+}
+
+String Network::decodeReceiveStates(receiveStates states) {
+    switch (states) {
+        case nothingReceived:
+            return "nothingReceived";
+            break;
+        case pickupLeft:
+            return "pickupLeft";
+            break;
+        case pickupRight:
+            return "pickupRight";
+            break;
+        case dropLeft:
+            return "dropLeft";
+            break;
+        case dropRight:
+            return "dropRight";
+            break;
+        default:
+            return "unknown";
+            break;
+    }
+}
+String Network::decodeSendStates(sendStates states) {
+    switch (states) {
+        case pickupSuccess:
+            return "pickupSuccess";
+            break;
+        case pickupFailure:
+            return "pickupFailure";
+            break;
+        case dropSuccess:
+            return "dropSuccess";
+            break;
+        case dropFailure:
+            return "dropFailure";
+            break;
+        default:
+            return "unknown";
+            break;
+    }
 }
--- lib/Network/Network.h
+++ lib/Network/Network.h
@@ -11,39 +11,90 @@
 #include "Adafruit_BluefruitLE_UART.h"
 
 #if SOFTWARE_SERIAL_AVAILABLE
-  #include <SoftwareSerial.h>
+#include <SoftwareSerial.h>
 #endif
 
-#define FACTORYRESET_ENABLE         1
+#define FACTORYRESET_ENABLE 1
 #define MINIMUM_FIRMWARE_VERSION "0.6.6"
 
-class Network{
-public:
-   enum receiveStates {
-    nothingReceived=-1, // no messages received
-	pickupLeft=0,	// pickup(0)
-	pickupRight=1,	// pickup(1)
-	dropLeft=2,		// drop(0)
-	dropRight=3		// drop(1)
-	};
-	enum sendStates {
-	pickupSuccess=0,	// pickupSuccess(1)
-	pickupFailure=1,	// pickupSuccess(0)
-	dropSuccess=2,		// dropSuccess(1)
-	dropFailure=3		// dropSuccess(0)
-	};
+class Network {
+   public:
+    enum receiveStates {
+        nothingReceived = -1,  // no messages received or unknown message received
+        pickupLeft = 0,        // pickup(0)
+        pickupRight = 1,       // pickup(1)
+        dropLeft = 2,          // drop(0)
+        dropRight = 3          // drop(1)
+    };
+    enum sendStates {
+        pickupSuccess = 0,  // pickupSuccess(1)
+        pickupFailure = 1,  // pickupSuccess(0)
+        dropSuccess = 2,    // dropSuccess(1)
+        dropFailure = 3     // dropSuccess(0)
+    };
 
-public:
-   Network(String name);
-   receiveStates receiveAndAnalyse();
-   bool sendMessage(sendStates state);
-   bool sendTestMessage(String msg);
-   void printInfo();   
-   
+   public:
+    /**
+    * @brief Construct a new Network object
+    * 
+    * @param name - 
+    */
+    Network();
+
+    /**
+     * @brief waits until connected to bluetooth
+     * 
+     * @param name - 
+     */
+    void init(String name);
+
+    /**
+     * @brief 
+     * 
+     * @return receiveStates - 
+     */
+    receiveStates receiveAndAnalyse();
+    /**
+     * @brief 
+     * 
+     * @param state - 
+     * @return true - 
+     * @return false - 
+     */
+    bool sendMessage(sendStates state);
+
+    /**
+     * @brief 
+     * 
+     * @param msg - 
+     * @return true - 
+     * @return false - 
+     */
+    bool sendTestMessage(String msg);
+
+    /**
+     * @brief 
+     * 
+     */
+    void printInfo();
+
+    /**
+     * @brief 
+     * 
+     * @return String - 
+     */
+    String decodeReceiveStates(receiveStates);
+
+    /**
+     * @brief 
+     * 
+     * @return String - 
+     */
+    String decodeSendStates(sendStates);
+
    private:
-    Adafruit_BluefruitLE_SPI *ble;
-    bool sendString(String msg);
+    Adafruit_BluefruitLE_SPI ble;  ///<
+    bool sendString(String msg);   ///<
 };
-
 
 #endif
